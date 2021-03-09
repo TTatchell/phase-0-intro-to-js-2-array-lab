fadfff71e95dc60e31d6690f0675fdd25472807f
TTatchell <tim.tatchell24@gmail.com>
2021-03-09 14:57:45 +1100

    upload

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+function logger() {
+    console.log(cats)
+}
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {cats.push(name)}
+
+function destructivelyPrependCat(name) {cats.unshift(name)}
+
+function destructivelyRemoveLastCat(name) {cats.pop(name)}
+
+function destructivelyRemoveFirstCat(name) {cats.shift(name)}
+
+function appendCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.push(name);
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(name);
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.pop(name);
+    return copyOfCats
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.shift(name);
+    return copyOfCats
+}
